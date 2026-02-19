ba34c4ad0bb943ce12393b883c206fff965331b2
Allan <allanvillatoro88@gmail.com>
2026-02-18 21:10:33 -0600

    Update dark mode colors and set body background to white

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
       </head>
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen bg-white">
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
