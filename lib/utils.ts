2cbbc38179eed257a8b0b31b7d4b09f92ed448b0
Allan <allanvillatoro88@gmail.com>
2026-03-24 14:28:34 -0600

    remove unneeded getImageUrl

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,12 +6,6 @@ export function formatPrice(price: number): string {
   return `L${price.toLocaleString('es-HN', { minimumFractionDigits: 0, maximumFractionDigits: 2 })}`;
 }
 
-const BASE_URL = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/`;
-
-export function getImageUrl(filename: string): string {
-  return `${BASE_URL}${filename}`;
-}
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
