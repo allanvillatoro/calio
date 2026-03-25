05d9b553b71b50b6f1764770512d1a93536ca029
Allan <allanvillatoro88@gmail.com>
2026-03-25 16:04:37 -0600

    add users repository

---
+++ lib/repositories/users-repository.interface.ts
@@ -0,0 +1,20 @@
+export interface IUser {
+  id: string;
+  email: string;
+}
+
+export interface UserCredentials {
+  email: string;
+  password: string;
+}
+
+export interface LoginResult {
+  user: IUser;
+  token: string;
+}
+
+export interface IUsersRepository {
+  register(credentials: UserCredentials): Promise<IUser>;
+  login(credentials: UserCredentials): Promise<LoginResult>;
+  checkAuthStatus(user: IUser): Promise<LoginResult>;
+}
