8cd47e0567d87e94a3ca7fae8fe6fddea3b06dd0
Allan <allanvillatoro88@gmail.com>
2026-04-01 18:30:53 -0600

    set cookies in login

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import { NextResponse } from 'next/server';
 import { ZodError } from 'zod';
 import { UserAuthenticationError } from '@/lib/errors';
+import { AUTH_TOKEN_COOKIE_NAME } from '@/lib/auth';
 import { formatZodError } from '@/lib/zod';
 import { userCredentialsSchema } from '../schemas';
 import { usersRepository } from '@/lib/repositories/users/drizzle-users-repository';
@@ -10,8 +11,19 @@ export async function POST(request: Request) {
   try {
     const body = userCredentialsSchema.parse(await request.json());
     const loginResult = await usersRepository.login(body);
+    const response = NextResponse.json(loginResult);
 
-    return NextResponse.json(loginResult);
+    response.cookies.set({
+      name: AUTH_TOKEN_COOKIE_NAME,
+      value: loginResult.token,
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      maxAge: 60 * 60 * 4,
+    });
+
+    return response;
   } catch (error) {
     if (error instanceof ZodError) {
       return NextResponse.json(formatZodError(error), {
