f6ae0b0fd40075c2275e9012c668f18858e47204
Allan <allanvillatoro88@gmail.com>
2026-04-10 14:51:58 -0600

    add index

--- db/schema.ts
+++ db/schema.ts
@@ -7,27 +7,35 @@ import {
   pgTable,
   text,
   timestamp,
+  uniqueIndex,
   uuid,
 } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
 
-export const products = pgTable('products', {
-  id: bigint('id', { mode: 'number' })
-    .primaryKey()
-    .generatedByDefaultAsIdentity(),
-  name: text('name').notNull(),
-  description: text('description').notNull(),
-  price: numeric('price', {
-    precision: 10,
-    scale: 2,
-    mode: 'number',
-  }).notNull(),
-  quantity: integer('quantity').notNull(),
-  images: jsonb('images').$type<string[]>().notNull(),
-  category: text('category').notNull(),
-  inStore: boolean('in_store').notNull().default(false),
-  createdAt: timestamp('created_at', { withTimezone: true }).notNull(),
-  updatedAt: timestamp('updated_at', { withTimezone: true }).notNull(),
-});
+export const products = pgTable(
+  'products',
+  {
+    id: bigint('id', { mode: 'number' })
+      .primaryKey()
+      .generatedByDefaultAsIdentity(),
+    name: text('name').notNull(),
+    description: text('description').notNull(),
+    price: numeric('price', {
+      precision: 10,
+      scale: 2,
+      mode: 'number',
+    }).notNull(),
+    quantity: integer('quantity').notNull(),
+    images: jsonb('images').$type<string[]>().notNull(),
+    category: text('category').notNull(),
+    inStore: boolean('in_store').notNull().default(false),
+    createdAt: timestamp('created_at', { withTimezone: true }).notNull(),
+    updatedAt: timestamp('updated_at', { withTimezone: true }).notNull(),
+  },
+  (table) => [
+    uniqueIndex('products_name_unique').on(sql`lower(trim(${table.name}))`),
+  ],
+);
 
 export const users = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
