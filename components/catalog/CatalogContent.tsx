2548e2200ecc95f4ff6acb872dad791861b3c965
Allan <allanvillatoro88@gmail.com>
2026-04-14 21:24:34 -0600

    update labels

--- components/catalog/CatalogContent.tsx
+++ components/catalog/CatalogContent.tsx
@@ -77,7 +77,7 @@ export default function CatalogContent() {
       return upperCategory === 'NEW IN' ? 'NUEVA COLECCIÓN' : upperCategory;
     }
     if (query) return `Resultados para "${query}"`;
-    if (inStore) return 'Catálogo en Tienda';
+    if (inStore) return 'Piezas en Tienda Física';
     return 'Catálogo Completo';
   };
 
