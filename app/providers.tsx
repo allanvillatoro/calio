ba37bce5a362e81866261fa810f948d80c16cb4f
Allan <allanvillatoro88@gmail.com>
2026-05-05 22:39:32 -0600

    Add AuthStateSync component for synchronizing authentication state

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 import { Toaster } from 'sonner';
+import { AuthStateSync } from '@/components/auth/AuthStateSync';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -23,6 +24,7 @@ export function Providers({ children }: ProvidersProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <AuthStateSync />
       {children}
       <Toaster position="top-right" richColors closeButton />
       <ReactQueryDevtools initialIsOpen={false} />
